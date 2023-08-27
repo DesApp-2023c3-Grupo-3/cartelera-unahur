2f38aca922a3dc7da9ffa2a700db37dc8d9fb821
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2023-08-27 12:41:49 -0300

    Orden

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from './dto/contacto';
 export * from './dto/sector';
 export * from './dto/schedule';
-export * from './dto/image';
 export * from './dto/course';
+export * from './dto/image';
