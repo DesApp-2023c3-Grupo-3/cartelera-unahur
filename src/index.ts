ff7d7c66985f2e0c934ea520778b8fdc57cf3cd7
ValentinoChiappanni <valentino.chiappanni@estudiantes.unahur.edu.ar>
2023-08-29 12:42:09 -0300

    Se corrige nombre image a imageType

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export * from './dto/contacto';
 export * from './dto/sector';
 export * from './dto/schedule';
 export * from './dto/course';
-export * from './dto/image';
+export * from './dto/image-type';
