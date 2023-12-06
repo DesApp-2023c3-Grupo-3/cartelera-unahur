c184b503df423222724e27e2f7c8902130355bda
tomas0011 <tomasbenjamin117@gmail.com>
2023-12-06 00:11:17 -0300

    changes

--- src/dto/screen/update-screen.dto.ts
+++ src/dto/screen/update-screen.dto.ts
@@ -1,3 +1,10 @@
+import { OmitType } from '../../nest/swagger/utils';
 import { ScreenDto } from './screen.dto';
 
-export class UpdateScreenDto extends ScreenDto {}
+export class UpdateScreenDto extends OmitType(ScreenDto, ['sector'] as const) {
+  sector: {
+    id: string,
+    topic: string,
+    name: string
+  };
+}
