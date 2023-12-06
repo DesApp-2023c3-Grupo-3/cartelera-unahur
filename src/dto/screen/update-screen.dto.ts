5c4fec932a931d49ed8defe499011bd591db0f20
tomas0011 <tomasbenjamin117@gmail.com>
2023-12-06 00:03:53 -0300

    types on update screen

--- src/dto/screen/update-screen.dto.ts
+++ src/dto/screen/update-screen.dto.ts
@@ -1,4 +1,3 @@
-import { OmitType } from '../../nest/swagger/utils';
 import { ScreenDto } from './screen.dto';
 
-export class UpdateScreenDto extends OmitType(ScreenDto, ['subscription', 'sector'] as const) {}
+export class UpdateScreenDto extends ScreenDto {}
